cd9d00763084760aa4503022ac9993164419f632
unknown <jan.rejnowski@gmail.com>
2017-06-06 18:28:29 +0200

    Some other lambda examples

--- src/com/janek/dao/Application.java
+++ src/com/janek/dao/Application.java
@@ -1,9 +1,13 @@
 package com.janek.dao;
 
+import com.janek.UserPredicateFactory;
 import com.janek.builder.User;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by RENT on 2017-06-05.
@@ -11,6 +15,27 @@ import java.util.List;
 public class Application {
     private static final String PATH = "C:\\Users\\RENT\\IdeaProjects\\projekt\\Wzorce-projektowe\\users";
     public static void main(String[] args) {
+        List<List<String>> nameList = Arrays.asList(
+                Arrays.asList("Szymon", "Wojciech", "Tomasz"),
+                Arrays.asList("Anna", "Janina", "Malgorzata"));
+
+        nameList.stream()
+                .flatMap(e -> e.stream())
+                .filter(e -> e.startsWith("A"))
+                .forEach(e -> System.out.println(e));
+
+        Map<String, List<String>> map = new HashMap<>();
+        map.put("men", Arrays.asList("Szymon", "Wojciech", "Adam"));
+        map.put("woman", Arrays.asList("Anna", "Janina", "Malgorzata"));
+
+        map.entrySet()
+                .stream()
+                .map(e -> e.getValue())
+                .flatMap(e -> e.stream())
+                .filter(UserPredicateFactory.nameStartsWith("A"))
+                .forEach(e -> System.out.println(e));
+
+
         UserDao userDao = new UserDaoInFile(new File(PATH));
         userDao.addUser(User.builder()
                         .firstName("Michal")
